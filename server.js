7c8dbc9e3232eab34e03af9a9ea1c58a26734e57
vladibo13 <vladibo13@gmail.com>
2022-01-21 17:34:44 +0200

    routes init

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 
+const bootcamps = require('./routes/bootcamps')
+app.use('/api/v1/bootcamps', bootcamps)
+
 dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 5000
 
