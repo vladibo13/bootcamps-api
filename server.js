70064c540dd7299df874296ec40c9523f3f311ea
vladibo13 <vladibo13@gmail.com>
2022-01-21 17:58:53 +0200

    added morgan loging middleware

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
+const morgan = require('morgan')
 
+// routes
 const bootcamps = require('./routes/bootcamps')
-app.use('/api/v1/bootcamps', bootcamps)
 
+//env variables
 dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 5000
+
+//dev logger middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+//mount routes
+app.use('/api/v1/bootcamps', bootcamps)
 
 app.listen(
   PORT,
