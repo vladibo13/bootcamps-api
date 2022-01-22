fdcef59a1bce49c4668b6856e66bbec8a6387d1f
vladibo13 <vladibo13@gmail.com>
2022-01-22 14:26:21 +0200

    mongodb database init

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // routes
 const bootcamps = require('./routes/bootcamps')
@@ -9,6 +10,9 @@ const bootcamps = require('./routes/bootcamps')
 //env variables
 dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 5000
+
+//connect to database
+connectDB()
 
 //dev logger middleware
 if (process.env.NODE_ENV === 'development') {
@@ -18,7 +22,16 @@ if (process.env.NODE_ENV === 'development') {
 //mount routes
 app.use('/api/v1/bootcamps', bootcamps)
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`REST API running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `REST API running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 )
+
+//Handle unhandled rejection
+process.on('unhandledRejection', (e, promise) => {
+  console.log(`Error: ${e.message}`)
+  // close the server exit process
+  server.close(() => process.exit(1))
+})
